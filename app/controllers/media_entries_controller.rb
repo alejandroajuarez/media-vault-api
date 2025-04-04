e82cebe96d31b763765200f3e3b3bcc42c4d8215
alejandrojuarez117 <aljuarez117@gmail.com>
2025-04-03 19:05:15 -0600

    added rack, bcrypt, jwt gems and CRUD to media_entries controller as well as Create function to users controller

--- app/controllers/media_entries_controller.rb
+++ app/controllers/media_entries_controller.rb
@@ -1,6 +1,48 @@
 class MediaEntriesController < ApplicationController
-
   def index
-    render json: {message: "hello"}
+    @mediaentries = MediaEntry.all
+    render :index
+  end
+
+  def show
+    id_find = find_by[:id]
+    @mediaentry = MediaEntry.find_by(id: id_find)
+    render :show
+  end
+
+  def create
+    @mediaentry = MediaEntry.new(
+      title: params[:title],
+      description: params[:description],
+      media_type: params[:media_type],
+      image_url: params[:image_url],
+      creator: params[:creator]
+    )
+    if @mediaentry.save
+      render :show
+    else
+      render json: { errors: @mediaentry.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @mediaentry = MediaEntry.find_by(id: params[:id])
+    @mediaentry.title = params[:title]              || @mediaentry.title
+    @mediaentry.description = params[:description]  || @mediaentry.description
+    @mediaentry.media_type = params[:media_type]    || @mediaentry.media_type
+    @mediaentry.image_url = params[:image_url]      || @mediaentry.image_url
+    @mediaentry.creator = params[:creator]          || @mediaentry.creator
+
+    if @mediaentry.save!
+      render :show
+    else
+      render json: { errors: @mediaentry.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    media_entry = MediaEntry.find_by(id: params[:id])
+    media_entry.destroy
+    render json: { message: "Entry has been deleted" }
   end
 end
