18b9cbe8d7efae85e3c39adca75cfac23c9a6c5e
alejandrojuarez117 <aljuarez117@gmail.com>
2025-04-09 20:27:37 -0600

    added media_status statuses to filter on

--- app/models/saved_medium.rb
+++ app/models/saved_medium.rb
@@ -2,4 +2,6 @@ class SavedMedium < ApplicationRecord
   belongs_to :user
   belongs_to :media_entry
   validates :media_entry_id, uniqueness: { scope: :user_id, message: "has already been saved" }
+
+  enum media_status: { saved: "saved", in_progress: "in_progress", archived: "archived" }
 end
